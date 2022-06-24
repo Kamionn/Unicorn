33e0faee64aeeb5862565a5e28ddf6dda733511c
Kamionn <mattheo.lefur41@gmail.com>
2022-06-24 11:39:50 +0200

    Update kcl_f6.lua

--- client/kcl_f6.lua
+++ client/kcl_f6.lua
@@ -22,88 +22,82 @@ function OpenMenuUnicorn(Job, BossAccessLabelRank, JobGrade, JobGradeName, MenuC
                                 TriggerServerEvent('::{Kamion#1323}::ServiceOff_unicorn')
                             end})
 
-            if serviceunicorn then
-                RageUI.Separator('____')
-                RageUI.List(_U('tablet'), unicornindex, unicornalue, nil, {}, true, {
-                    onListChange = function(Index)
-                        unicornalue = Index
-                        unicornalue = Index;
+                if serviceunicorn then
+                    RageUI.Separator('____')
+                    RageUI.List(_U('tablet'), unicornindex, unicornalue, nil, {}, true, {
+                        onListChange = function(Index)
+                            unicornalue = Index
+                            unicornalue = Index;
+                        end})
+                    RageUI.Separator('____')
+
+                if unicornalue == 1 then
+                    RageUI.Button(_U('announcement_open'), nil, {RightLabel = "→"}, not codesCooldown1, {
+                        onSelected = function()
+                            codesCooldown1 = true 
+                                TriggerServerEvent('::{Kamion#1323}::Open:unicorn')
+                        Citizen.SetTimeout(8000, function() codesCooldown1 = false end)
                     end})
-                RageUI.Separator('____')
+                    
+                    RageUI.Button(_U('announcement_recruitment'), nil, {RightLabel = "→"}, not codesCooldown3, {
+                        onSelected = function()
+                            codesCooldown3 = true 
+                                TriggerServerEvent('::{Kamion#1323}::Recru:unicorn')
+                        Citizen.SetTimeout(8000, function() codesCooldown3 = false end)
+                    end})
 
-            if unicornalue == 1 then
-                RageUI.Button(_U('announcement_open'), nil, {RightLabel = "→"}, not codesCooldown1, {
-                    onSelected = function()
-                        codesCooldown1 = true 
-                            if ESX.PlayerData.job2.name == Config.job.unicorn or ESX.PlayerData.job.name == Config.job.unicorn then
-                            TriggerServerEvent('::{Kamion#1323}::Open:unicorn')
-                        end
-                    Citizen.SetTimeout(8000, function() codesCooldown1 = false end)
-                end})
-                
-                RageUI.Button(_U('announcement_recruitment'), nil, {RightLabel = "→"}, not codesCooldown3, {
-                     onSelected = function()
-                        codesCooldown3 = true 
-                            if ESX.PlayerData.job2.name == Config.job.unicorn or ESX.PlayerData.job.name == Config.job.unicorn then
-                            TriggerServerEvent('::{Kamion#1323}::Recru:unicorn')
-                        end
-                    Citizen.SetTimeout(8000, function() codesCooldown3 = false end)
-                end})
+                    RageUI.Button(_U('announcement_close'), nil, {RightLabel = "→"}, not codesCooldown2, {
+                        onSelected = function()
+                            codesCooldown2 = true 
+                                TriggerServerEvent('::{Kamion#1323}::Close:unicorn')
+                        Citizen.SetTimeout(8000, function() codesCooldown2 = false end)
+                    end})        
 
-                 RageUI.Button(_U('announcement_close'), nil, {RightLabel = "→"}, not codesCooldown2, {
-                    onSelected = function()
-                        codesCooldown2 = true 
-                            if ESX.PlayerData.job2.name == Config.job.unicorn or ESX.PlayerData.job.name == Config.job.unicorn then
-                            TriggerServerEvent('::{Kamion#1323}::Close:unicorn')
-                        end
-                    Citizen.SetTimeout(8000, function() codesCooldown2 = false end)
-                end})        
-
-            elseif unicornalue == 2 then
-                local closestPlayer, closestDistance = ESX.Game.GetClosestPlayer()
-                  RageUI.Button(_U('billing'), nil, {RightLabel = "→→"}, true , {
-                    onSelected = function()
-                        amount = KeyboardInput(_U('billing_keyboard'),nil,8)
-                            amount = tonumber(amount)
-                                local player, distance = ESX.Game.GetClosestPlayer()
-                                    if player ~= -1 and distance <= 3.0 then
-                                        if amount == nil then
-                                            ESX.ShowNotification(_U('billing_error'))
-                                                else
-                                                    local playerPed = PlayerPedId() 
-                                                        Citizen.Wait(500)
-                                                            for i = 1, #Config.unicorn do
-                                                                local v = Config.unicorn[i]
-                                                        TriggerServerEvent('esx_billing:sendBill', GetPlayerServerId(player), v.JobSociety, ('unicorn'), amount)
-                                                    end
-                                                Citizen.Wait(100)
-                                            ESX.ShowNotification(_U('billing_sucess'))
-                                        end
-                                    else
-                                ESX.ShowNotification(_U('billing_noplayer'))
-                            end
-                        end});
-
-                RageUI.Button(_U('recruit'), nil , {RightLabel = "→→"}, closestPlayer ~= -1 and closestDistance <= 5.0,{
-                    onSelected = function()     
-                        if closestPlayer == -1 or closestDistance > 5.0 then
-                            ESX.ShowNotification(_U('recruit_noplayer'))
-                                else
-                                    for i = 1, #Config.unicorn do
-                                        local v = Config.unicorn[i]            
-                                        TriggerServerEvent('esx_society:manageJob', GetPlayerServerId(closestPlayer), v.JobSociety, "recrutement", 0)
-                                    end
+                elseif unicornalue == 2 then
+                    local closestPlayer, closestDistance = ESX.Game.GetClosestPlayer()
+                    RageUI.Button(_U('billing'), nil, {RightLabel = "→→"}, true , {
+                        onSelected = function()
+                            amount = KeyboardInput(_U('billing_keyboard'),nil,8)
+                                amount = tonumber(amount)
+                                    local player, distance = ESX.Game.GetClosestPlayer()
+                                        if player ~= -1 and distance <= 3.0 then
+                                            if amount == nil then
+                                                ESX.ShowNotification(_U('billing_error'))
+                                                    else
+                                                        local playerPed = PlayerPedId() 
+                                                            Citizen.Wait(500)
+                                                                for i = 1, #Config.unicorn do
+                                                                    local v = Config.unicorn[i]
+                                                            TriggerServerEvent('esx_billing:sendBill', GetPlayerServerId(player), v.JobSociety, ('unicorn'), amount)
+                                                        end
+                                                    Citizen.Wait(100)
+                                                ESX.ShowNotification(_U('billing_sucess'))
+                                            end
+                                        else
+                                    ESX.ShowNotification(_U('billing_noplayer'))
                                 end
                             end});
+
+                    RageUI.Button(_U('recruit'), nil , {RightLabel = "→→"}, closestPlayer ~= -1 and closestDistance <= 5.0,{
+                        onSelected = function()     
+                            if closestPlayer == -1 or closestDistance > 5.0 then
+                                ESX.ShowNotification(_U('recruit_noplayer'))
+                                    else
+                                        for i = 1, #Config.unicorn do
+                                            local v = Config.unicorn[i]            
+                                            TriggerServerEvent('esx_society:manageJob', GetPlayerServerId(closestPlayer), v.JobSociety, "recrutement", 0)
+                                        end
+                                    end
+                                end});
+                            end
                         end
-                    end
-                end)
-              if not RageUI.Visible(menuUnicorn) then
-           FreezeEntityPosition(PlayerPedId(), false)
-          menuUnicorn = RMenu:DeleteType("menuUnicorn", true)
+                    end)
+                if not RageUI.Visible(menuUnicorn) then
+            FreezeEntityPosition(PlayerPedId(), false)
+            menuUnicorn = RMenu:DeleteType("menuUnicorn", true)
+            end
         end
     end
-end
 
 
 
