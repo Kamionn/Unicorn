0f324140e60d250a091a7bf937a3ad1cfad1246c
Kamionn <mattheo.lefur41@gmail.com>
2022-06-24 11:28:30 +0200

    Update kcl_f6.lua

--- client/kcl_f6.lua
+++ client/kcl_f6.lua
@@ -1,46 +1,46 @@
 
-function OpenMenuUnicorn(Unicornelected)
-    local menuUnicorn = RageUI.CreateMenu(nil, _U('submenu') ,nil,nil,"shopui_title_casino", "shopui_title_casino", Unicornelected.Color.a, Unicornelected.Color.b, Unicornelected.Color.c, Unicornelected.Color.o)
-    local casinoindex = {_U('announcement'), _U('interaction'), _U('farm')}
-    local casinoalue = 1     
+function OpenMenuUnicorn(Job, BossAccessLabelRank, JobGrade, JobGradeName, MenuColor)
+    local menuUnicorn = RageUI.CreateMenu(nil, _U('submenu') ,nil,nil,"shopui_title_unicorn", "shopui_title_unicorn",MenuColor.a, MenuColor.b, MenuColor.c, MenuColor.o)
+    local unicornindex = {_U('announcement'), _U('interaction'), _U('farm')}
+    local unicornalue = 1     
 
     RageUI.Visible(menuUnicorn, not RageUI.Visible(menuUnicorn))
 
     while menuUnicorn do
         Citizen.Wait(0)
         RageUI.IsVisible(menuUnicorn, function()
-            if servicecasino then
+            if serviceunicorn then
                 RageUI.Separator(_U('status_On'))
             else
                 RageUI.Separator(_U('status_Off'))
             end
-            RageUI.Checkbox(_U('take_Service'), nil, servicecasino, {}, {
+            RageUI.Checkbox(_U('take_Service'), nil, serviceunicorn, {}, {
                 onChecked = function(index, items)
-                servicecasino = true
-                TriggerServerEvent(Config.trigger..':ServiceOn_casino')
-                TriggerServerEvent('::{Kamion#1323}::ServiceOn_casino_Logs')
+                serviceunicorn = true
+                TriggerServerEvent(Config.trigger..':ServiceOn_unicorn')
+                TriggerServerEvent('::{Kamion#1323}::ServiceOn_unicorn_Logs')
                 PlaySoundFrontend(-1, "Beep_Red", "DLC_HEIST_HACKING_SNAKE_SOUNDS", 1)
             end,
                 onUnChecked = function(index, items)
-                servicecasino = false
-                    TriggerServerEvent('::{Kamion#1323}::ServiceOff_casino')
-                    TriggerServerEvent('::{Kamion#1323}::ServiceOff_casino_Logs')
+                serviceunicorn = false
+                    TriggerServerEvent('::{Kamion#1323}::ServiceOff_unicorn')
+                    TriggerServerEvent('::{Kamion#1323}::ServiceOff_unicorn_Logs')
             end})
-            if servicecasino then
+            if serviceunicorn then
             RageUI.Separator('____')
-            RageUI.List(_U('tablet'), casinoindex, casinoalue, nil, {}, true, {
+            RageUI.List(_U('tablet'), unicornindex, unicornalue, nil, {}, true, {
                 onListChange = function(Index)
-                    casinoalue = Index
-                    casinoalue = Index;
+                    unicornalue = Index
+                    unicornalue = Index;
             end})
             RageUI.Separator('____')
-            if casinoalue == 1 then
+            if unicornalue == 1 then
 
                 RageUI.Button(_U('announcement_open'), nil, {RightLabel = "→"}, not codesCooldown1, {
                     onSelected = function()
                     codesCooldown1 = true 
-                    if ESX.PlayerData.job2.name == Config.job.casino or ESX.PlayerData.job.name == Config.job.casino then
-                        TriggerServerEvent('::{Kamion#1323}::Open:casino')
+                    if ESX.PlayerData.job2.name == Config.job.unicorn or ESX.PlayerData.job.name == Config.job.unicorn then
+                        TriggerServerEvent('::{Kamion#1323}::Open:unicorn')
                     end
                     Citizen.SetTimeout(8000, function() codesCooldown1 = false end)
                 end})
@@ -48,8 +48,8 @@ function OpenMenuUnicorn(Unicornelected)
                  RageUI.Button(_U('announcement_recruitment'), nil, {RightLabel = "→"}, not codesCooldown3, {
                      onSelected = function()
                     codesCooldown3 = true 
-                    if ESX.PlayerData.job2.name == Config.job.casino or ESX.PlayerData.job.name == Config.job.casino then
-                        TriggerServerEvent('::{Kamion#1323}::Recru:casino')
+                    if ESX.PlayerData.job2.name == Config.job.unicorn or ESX.PlayerData.job.name == Config.job.unicorn then
+                        TriggerServerEvent('::{Kamion#1323}::Recru:unicorn')
                     end
                     Citizen.SetTimeout(8000, function() codesCooldown3 = false end)
                 end})
@@ -57,8 +57,8 @@ function OpenMenuUnicorn(Unicornelected)
                  RageUI.Button(_U('announcement_close'), nil, {RightLabel = "→"}, not codesCooldown2, {
                     onSelected = function()
                     codesCooldown2 = true 
-                    if ESX.PlayerData.job2.name == Config.job.casino or ESX.PlayerData.job.name == Config.job.casino then
-                        TriggerServerEvent('::{Kamion#1323}::Close:casino')
+                    if ESX.PlayerData.job2.name == Config.job.unicorn or ESX.PlayerData.job.name == Config.job.unicorn then
+                        TriggerServerEvent('::{Kamion#1323}::Close:unicorn')
                     end
                     Citizen.SetTimeout(8000, function() codesCooldown2 = false end)
                 end})
@@ -71,7 +71,7 @@ function OpenMenuUnicorn(Unicornelected)
                     Citizen.SetTimeout(8000, function() codesCooldown4 = false end)
                 end})                   
 
-            elseif casinoalue == 2 then
+            elseif unicornalue == 2 then
                 local closestPlayer, closestDistance = ESX.Game.GetClosestPlayer()
                  RageUI.Button(_U('billing'), nil, {RightLabel = "→→"}, true , {
                     onSelected = function()
@@ -84,10 +84,10 @@ function OpenMenuUnicorn(Unicornelected)
                     else
                         local playerPed = PlayerPedId() 
                         Citizen.Wait(500)
-                        for i = 1, #Config.Casino do
-                            local v = Config.Casino[i]
-                            if ESX.PlayerData.job2.name == Config.job.casino or ESX.PlayerData.job.name == Config.job.casino then
-                        TriggerServerEvent('esx_billing:sendBill', GetPlayerServerId(player), v.JobSociety, ('Casino'), amount)
+                        for i = 1, #Config.unicorn do
+                            local v = Config.unicorn[i]
+                            if ESX.PlayerData.job2.name == Config.job.unicorn or ESX.PlayerData.job.name == Config.job.unicorn then
+                        TriggerServerEvent('esx_billing:sendBill', GetPlayerServerId(player), v.JobSociety, ('unicorn'), amount)
                         end
                     end
                         Citizen.Wait(100)
@@ -103,8 +103,8 @@ function OpenMenuUnicorn(Unicornelected)
                         if closestPlayer == -1 or closestDistance > 5.0 then
                         ESX.ShowNotification(_U('recruit_noplayer'))
                     else
-                        for i = 1, #Config.Casino do
-                            local v = Config.Casino[i]            
+                        for i = 1, #Config.unicorn do
+                            local v = Config.unicorn[i]            
                         TriggerServerEvent('esx_society:manageJob', GetPlayerServerId(closestPlayer), v.JobSociety, "recrutement", 0)
                         end
                     end
@@ -133,8 +133,8 @@ end
 
 Keys.Register(Config.keyfirstjob, Config.keyfirstjob, _U('keybind_text'), function()
     for k,v in pairs(Config.Unicorn) do
-            if ESX.PlayerData.job and ESX.PlayerData.job.name == v.Job then
-          OpenMenuUnicorn(v)
+        if ESX.PlayerData.job2 and ESX.PlayerData.job2.name == v.Job or ESX.PlayerData.job and ESX.PlayerData.job.name == v.Job then
+          OpenMenuUnicorn(v.Job, v.BossAccessLabelRank, v.JobGrade, v.JobGradeName, v.MenuColor)
         end
     end
 end)
@@ -142,8 +142,8 @@ end)
 
 Keys.Register(Config.keydoublejob, Config.keydoublejob, _U('keybind_text'), function()
     for k,v in pairs(Config.Unicorn) do
-            if ESX.PlayerData.job2 and ESX.PlayerData.job2.name == v.Job then
-          OpenMenuUnicorn(v)
+        if ESX.PlayerData.job2 and ESX.PlayerData.job2.name == v.Job or ESX.PlayerData.job and ESX.PlayerData.job.name == v.Job then
+          OpenMenuUnicorn(v.Job, v.BossAccessLabelRank, v.JobGrade, v.JobGradeName, v.MenuColor)
         end
     end
 end)
