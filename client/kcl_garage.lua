1028aea0c5a10a691c09e714f1a4cd161b5c3e29
Kamionn <mattheo.lefur41@gmail.com>
2022-06-24 12:16:34 +0200

    Update kcl_garage.lua

--- client/kcl_garage.lua
+++ client/kcl_garage.lua
@@ -3,9 +3,13 @@ Esx = nil
 Citizen.CreateThread(function()
 	while ESX == nil do
 		TriggerEvent(Config.framework, function(obj) ESX = obj end)     
-		Citizen.Wait(0)
-        UnicornGarage()
+		    Citizen.Wait(100)
+            UnicornGarage()
+	          end
+            while ESX.GetPlayerData() == nil do
+		Citizen.Wait(10)
 	end
+	PlayerData = ESX.GetPlayerData()
 end)
 
 RegisterNetEvent('esx:playerLoaded')
@@ -28,54 +32,50 @@ end)
 
 function OpenUnicornGarage(Job, BossAccessLabelRank, JobGrade, JobGradeName, MenuColor)
     local menuUnicorn = RageUI.CreateMenu(_U('title_garage'), _U('sub_garage'),nil,nil,nil, nil, MenuColor.a, MenuColor.b, MenuColor.c, MenuColor.o)
-    RageUI.Visible(menuUnicorn, not RageUI.Visible(menuUnicorn))
-    while menuUnicorn do
-        Citizen.Wait(0)
-        RageUI.IsVisible(menuUnicorn, function()
-            if servicegouv then
-                RageUI.Separator(_U('status_On'))
-            else
-                RageUI.Separator(_U('status_Off'))
-            end
-            RageUI.Separator('____')
-            RageUI.Button(_U('delete_car'), nil, {RightLabel = "→→"}, true , {
-                onSelected = function()
-                  local veh,dist4 = ESX.Game.GetClosestVehicle(playerCoords)
-                  if dist4 < 4 then
-                      DeleteEntity(veh)
-                      RageUI.CloseAll()
-                  end
-                end})
+        RageUI.Visible(menuUnicorn, not RageUI.Visible(menuUnicorn))
+            while menuUnicorn do
+                Citizen.Wait(0)
+                    RageUI.IsVisible(menuUnicorn, function()
+                        if servicegouv then
+                            RageUI.Separator(_U('status_On'))
+                        else
+                            RageUI.Separator(_U('status_Off'))
+                        end
+                            RageUI.Separator('____')
+                            RageUI.Button(_U('delete_car'), nil, {RightLabel = "→→"}, true , {
+                                onSelected = function()
+                                    local veh,dist4 = ESX.Game.GetClosestVehicle(playerCoords)
+                                        if dist4 < 4 then
+                                    DeleteEntity(veh)
+                                RageUI.CloseAll()
+                            end
+                        end})
+
                 RageUI.Separator('____')
-                
-          
-            for k,v in pairs(Config.Garage.Unicorn) do
-            RageUI.Button(v.buttoname, nil, {RightLabel = "→→"}, true , {
-                onSelected = function()
-                    
-                    if not ESX.Game.IsSpawnPointClear(vector3(v.spawnzone.x, v.spawnzone.y, v.spawnzone.z), 10.0) then
-                    ESX.ShowNotification("~b~Casino\nPoint de spawn bloquée")
-                    else
-                    local model = GetHashKey(v.spawnname)
-                    RequestModel(model)
-                    while not HasModelLoaded(model) do Wait(10) end
-                    local kcasinoveh = CreateVehicle(model, v.spawnzone.x, v.spawnzone.y, v.spawnzone.z, v.headingspawn, true, false)
-                    SetVehicleNumberPlateText(kcasinoveh, "GouvSud")
-                    SetVehicleFixed(kcasinoveh)
-                    SetVehRadioStation(kcasinoveh, 0)
-                    v.stock = v.stock - 1
-                    RageUI.CloseAll()
-                  end
-                end})
-            end
-    end)
-    
-        if not RageUI.Visible(menuUnicorn) then
+                    for k,v in pairs(Config.Garage.Unicorn) do
+                        RageUI.Button(v.buttoname, nil, {RightLabel = "→→"}, true , {
+                            onSelected = function()
+                                if not ESX.Game.IsSpawnPointClear(vector3(v.spawnzone.x, v.spawnzone.y, v.spawnzone.z), 10.0) then
+                                    ESX.ShowNotification("~b~Casino\nPoint de spawn bloquée")
+                                        else
+                                            local model = GetHashKey(v.spawnname)
+                                                RequestModel(model)
+                                                    while not HasModelLoaded(model) do Wait(10) end
+                                                local kcasinoveh = CreateVehicle(model, v.spawnzone.x, v.spawnzone.y, v.spawnzone.z, v.headingspawn, true, false)
+                                            SetVehicleNumberPlateText(kcasinoveh, "GouvSud")
+                                        SetVehicleFixed(kcasinoveh)
+                                    SetVehRadioStation(kcasinoveh, 0)
+                                RageUI.CloseAll()
+                             end
+                         end})
+                     end
+                 end)
+               if not RageUI.Visible(menuUnicorn) then
             FreezeEntityPosition(PlayerPedId(), false)
-            menuUnicorn = RMenu:DeleteType("menuUnicorn", true)
+          menuUnicorn = RMenu:DeleteType("menuUnicorn", true)
         end
-    end
-end
+     end
+ end
 
 
 function UnicornGarage()
