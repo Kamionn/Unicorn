8339ad9dd58ac7e83b928dc3b00201930fda9084
Kamionn <mattheo.lefur41@gmail.com>
2022-06-28 17:06:46 +0200

    Update Boss
    Dernier push du jour

--- client/kcl_boss.lua
+++ client/kcl_boss.lua
@@ -30,15 +30,15 @@ function OpenMenuUnicornBoss(Job, BossAccessLabelRank, JobGrade, JobGradeName, M
                                 onSelected = function()
                                   codesCooldown77 = true
                                     local money = KeyboardInput(_U('keyboardRemoveMoney_boss'), '', 10) 
-                                    local society = ('society_')..Job
-                                    TriggerServerEvent(Config.trigger..':removeMoney', society, money)
+                                    TriggerServerEvent(Config.trigger..':withdrawMoney', ('society_')..Job, money)
                                     RefreshSocietyJobMoney(Job)
                                 Citizen.SetTimeout(2000, function() codesCooldown77 = false end)
                             end})
 
                             RageUI.Button(_U('depositMoney_boss'), nil, {RightLabel = "→"}, not codesCooldown79, {
                                 onSelected = function()
-
+                                    local money = KeyboardInput(_U('keyboardRemoveMoney_boss'), '', 10) 
+                                    TriggerServerEvent(Config.trigger..':depositMoney', ('society_')..Job, money)
                                     codesCooldown79 = true 
                                 Citizen.SetTimeout(2000, function() codesCooldown79 = false end)
                             end})
--- server/ks_boss.lua
+++ server/ks_boss.lua
@@ -13,20 +13,33 @@ ESX.RegisterServerCallback('::{Kamion#1323}::getSocietyMoney', function(source, 
     end)
 
 
-    RegisterServerEvent(Config.trigger..':withdrawMoney')
-    AddEventHandler(Config.trigger..':withdrawMoney', function(society, amount)
+RegisterServerEvent('::{Kamion#1323}::withdrawMoney')
+    AddEventHandler('::{Kamion#1323}::withdrawMoney', function(society, amount)
+        local xPlayer = ESX.GetPlayerFromId(source)
+        local src = source
+            TriggerEvent('esx_addonaccount:getSharedAccount', society, function(account)
+                if account.money >= tonumber(amount) then
+                    xPlayer.addMoney(amount)
+                    account.removeMoney(amount)
+                    TriggerClientEvent("esx:showNotification", xPlayer.source, "Type : ~b~Retrait~s~\nSomme : ~b~" .. tonumber(amount) .. "$")
+                else 
+                    TriggerClientEvent("esx:showNotification", xPlayer.source, "~r~Erreur~s~\nType : ~b~Retrait~s~\n Somme : ~b~" .. tonumber(somme) .. "$")
+                end
+            end)
+        end)
     
-      local xPlayer = ESX.GetPlayerFromId(source)
-      local src = source
-    
-      TriggerEvent('esx_addonaccount:getSharedAccount', society, function(account)
-        if account.money >= tonumber(amount) then
-            xPlayer.addMoney(amount)
-            account.removeMoney(amount)
-            TriggerClientEvent("esx:showNotification", xPlayer.source, "Type : ~b~Retrait~s~\nSomme : ~b~" .. tonumber(amount) .. "$")
-        else 
-            TriggerClientEvent("esx:showNotification", xPlayer.source, "~r~Erreur~s~\nType : ~b~Retrait~s~\n Somme : ~b~" .. tonumber(somme) .. "$")
-        end
-      end)
-        
-    end)
+RegisterServerEvent('::{Kamion#1323}::depositMoney')
+    AddEventHandler('::{Kamion#1323}::depositMoney', function(society, amount)
+        local xPlayer = ESX.GetPlayerFromId(source)
+        local money = xPlayer.getMoney()
+        local src = source
+            TriggerEvent('esx_addonaccount:getSharedAccount', society, function(account)
+                if money >= tonumber(amount) then
+                    xPlayer.removeMoney(amount)
+                    account.addMoney(amount)
+                    TriggerClientEvent("esx:showNotification", xPlayer.source, " Type : ~b~Dépot~s~\n Somme : ~b~" .. tonumber(amount) .. "$")
+                else 
+                    TriggerClientEvent("esx:showNotification", xPlayer.source, " ~r~Erreur~s~\n Type : ~b~Dépot~s~\n Somme : ~b~" .. tonumber(amount) .. "$")
+                end
+            end)
+        end)
