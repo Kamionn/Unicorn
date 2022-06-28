37fd34caffb2d80a51573cc59cf6317126be3fea
Kamionn <mattheo.lefur41@gmail.com>
2022-06-28 10:06:58 +0200

    Update kcl_chest.lua

--- client/kcl_chest.lua
+++ client/kcl_chest.lua
@@ -81,34 +81,32 @@ function OpenMenuDeposit(Job, BossAccessLabelRank, JobGrade, JobGradeName, MenuC
    end
 end
 
-function OpenMenugouvernementChestdeposit()
+function OpenMenuTake(Job, BossAccessLabelRank, JobGrade, JobGradeName, MenuColor)
     getInventory()
-    InZone = false
-    local deposit = RageUI.CreateMenu(gouvernementSelected.JobName, _U('submenu'), nil, nil, nil, nil, gouvernementSelected.Color.a, gouvernementSelected.Color.b, gouvernementSelected.Color.c, gouvernementSelected.Color.o)
-    RageUI.Visible(deposit, not RageUI.Visible(deposit))
-    while deposit do
-        Citizen.Wait(0)
-        RageUI.IsVisible(deposit, function()
-            if servicegouv then
-                RageUI.Separator(_U('status_On'))
-            else
-                RageUI.Separator(_U('status_Off'))
-            end
-            RageUI.Separator('____')
-            for k,v in pairs(all_items) do
-                RageUI.Button(v.label, nil, {RightLabel = "~b~x"..v.nb}, true, {onSelected = function()
-                    local count = KeyboardInput(_U('keyboard_take'),nil,10)
-                    count = tonumber(count)
-                    society = gouvernementSelected.JobSociety
-                    TriggerServerEvent("::{Kamion#1323}::putStockItems",v.item, count, society)
-                     TriggerServerEvent("::{Kamion#1323}::DepoSockItemsLogs",v.item, count)
-                    getInventory()
-                end});
-            end
-    end)      
-        if not RageUI.Visible(deposit, deposit) then
+    local menuUnicorn = RageUI.CreateMenu(_U('title_chest'), _U('sub_chest'),nil,nil,nil, nil, MenuColor.a, MenuColor.b, MenuColor.c, MenuColor.o)
+    RageUI.Visible(menuUnicorn, not RageUI.Visible(menuUnicorn))
+        while menuUnicorn do
+            Citizen.Wait(0)
+                RageUI.IsVisible(menuUnicorn, function()
+                    if serviceunicorn then
+                        RageUI.Separator(_U('status_On'))
+                    else
+                        RageUI.Separator(_U('status_Off'))
+                    end
+                        RageUI.Separator('____')
+                        for k,v in pairs(all_items) do
+                            RageUI.Button(v.label, nil, {RightLabel = "~p~x"..v.nb}, true, {onSelected = function()
+                                local count = KeyboardInput(_U('keyboard_take'),nil,10)
+                                count = tonumber(count)
+                                TriggerServerEvent(Config.trigger..':putStockItems',v.item, count, Job)
+                                getInventory()
+                            end});
+                        end
+                    end)      
+                if not RageUI.Visible(menuUnicorn, test) then
             FreezeEntityPosition(PlayerPedId(), false)
-            deposit = RMenu:DeleteType("deposit", true)
-        end
-    end
+        menuUnicorn = RMenu:DeleteType("menuUnicorn", true)
+      end
+   end
 end
+
